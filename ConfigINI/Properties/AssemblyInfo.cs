4ddcf84ff44088eb387d434e244a1a85315b4dc8
HappyBiscuits <luke@nowwireless.com>
2016-08-13 12:35:15 +0100

    Nuget

--- ConfigINI/Properties/AssemblyInfo.cs
+++ ConfigINI/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ConfigINI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A generic INI based configuration system, building off INIFileParser by Ricardo Amores Hernandez.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ConfigINI")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.1048")]
+[assembly: AssemblyVersion("1.0.2.1126")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
